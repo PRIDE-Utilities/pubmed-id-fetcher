1110209d7e2a5f047678288f842d51bf9887f565
acsordas <acsordas@84e480dd-4284-0410-bcb6-dc83b1e7cf5b>
2011-08-02 09:54:03 +0000

    working prototype, you supply a pmid it returns a docsum in plain text

--- src/main/java/Eutils.java
+++ src/main/java/Eutils.java
@@ -12,20 +12,19 @@ import org.apache.commons.httpclient.HttpStatus;
 import org.apache.commons.httpclient.NameValuePair;
 import org.apache.commons.httpclient.methods.GetMethod;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.logging.Logger;
-// import org.omg.CORBA.NameValuePair;
+
 
 public class Eutils {
 
     public static Logger logger = Logger.getLogger(Eutils.class.getName());
-    //private static String response;
-
-    //private static String pubmedId;
 
     // http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=pubmed&id=15852344&report=docsum&mode=text
 
@@ -35,21 +34,28 @@ public class Eutils {
         Map<String, String> parameters = new HashMap();
 
 
-        parameters.put("db","pubmed");
-        parameters.put("id","15852344");
-        parameters.put("report","docsum");
-        parameters.put("mode","text");
+        parameters.put("db", "pubmed");
+        parameters.put("id", pubmedId);
+        parameters.put("report", "docsum");
+        parameters.put("mode", "text");
 
-        // call make get request
-
-        //pubmedId.makeGetRequest();
+        // call makeGetRequest on base url + parameters
 
         InputStream response = makeGetRequest(baseUrl, parameters);
 
 
-        // get the summary out from input stream
+        // read from input stream
 
+        BufferedReader reader = new BufferedReader(new InputStreamReader(response));
+        String line = null;
 
+        try {
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
         return null;
     }
@@ -86,61 +92,10 @@ public class Eutils {
         return null;
     }
 
-	//    private static List<String> parsePmids(InputStream response)
-	//            throws XMLStreamException, FactoryConfigurationError {
-	//        List<String> pmids = new ArrayList<String>();
-	//
-	//        XMLStreamReader parser = XMLInputFactory.newInstance()
-	//                .createXMLStreamReader(response);
-	//        while (parser.hasNext()) {
-	//            parser.next();
-	//            if (parser.isStartElement() && parser.getLocalName().equals("Id")) {
-	//                parser.next();
-	//                String pmid = parser.getText();
-	//                pmids.add(pmid);
-	//            }
-	//        }
-	//        return pmids;
-	//    }
+    public static void main(String[] args) {
 
-    // TODO pull the article title too not just pmid
-
-	//}
-
-	// String query = "Lord[Author] AND semantic similarity[Title] AND Semantics[MeSH Major Topic]";
-
-
-	// String tester = (String) iterator.next();
-
-	//String tester = dbc.getContactkeyaccessionvalue().keySet().toString();
-
-	// to get the keys one by one for(String key : dbc.getContactkeyaccessionvalue().keySet())
-
-	//String query = "Csordas[Author] AND mitochondrial transfer[Title]";
-
-	public static void main(String[] args) {
-		//Map<String, String> params = new HashMap<String, String>();
-		//String baseUrl = "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi";
-		//String query = "Lord[Author] AND semantic similarity[Title] AND Semantics[MeSH Major Topic]";
-
-		//params.put("db", "pubmed");
-		//params.put("term", query);
-		//InputStream response = makeGetRequest(baseUrl, params);
-
-
-        //System.out.println(getPubMedSummary(pubmedId));
-
-		//		try {
-		//			List<String> pmids = parsePmids(response);
-		//			System.out.println(pmids);
-		//		} catch (XMLStreamException e) {
-		//			// TODO Auto-generated catch block
-		//			e.printStackTrace();
-		//		} catch (FactoryConfigurationError e) {
-		//			// TODO Auto-generated catch block
-		//			e.printStackTrace();
-		//		}
-	}
+        getPubMedSummary("15852344");
+    }
 
 }
 
