d6d946db065daf46308438399b0e728233d8e223
jdianes <jdianes@84e480dd-4284-0410-bcb6-dc83b1e7cf5b>
2012-08-14 08:31:05 +0000

    Refactoring and testing done

---
+++ src/test/java/uk/ac/ebi/pride/pubmed/integration/PubMedFetcherActualServiceIntegrationTest.java
@@ -0,0 +1,42 @@
+package uk.ac.ebi.pride.pubmed.integration;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import uk.ac.ebi.pride.pubmed.PubMedFetcher;
+import uk.ac.ebi.pride.pubmed.model.PubMedSummary;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+/**
+ * @author Jose A. Dianes
+ * @version $Id$
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"/pubmed-eutils-actual-context.xml"})
+public class PubMedFetcherActualServiceIntegrationTest {
+
+    // good examples of actual PUBMED_IDs
+    public static final String PUBMED_ID_1 = "22147733";
+    public static final String PUBMED_ID_2 = "22807455";
+
+    @Autowired
+    private PubMedFetcher pubMedFetcher;
+
+
+    @Test
+    public void testActualService() throws Exception {
+
+        PubMedSummary response = pubMedFetcher.getPubMedSummary(PUBMED_ID_1);
+
+        assertThat(response == null, is(false));
+
+
+        response = pubMedFetcher.getPubMedSummary(PUBMED_ID_2);
+
+        assertThat(response == null, is(false));
+    }
+}
