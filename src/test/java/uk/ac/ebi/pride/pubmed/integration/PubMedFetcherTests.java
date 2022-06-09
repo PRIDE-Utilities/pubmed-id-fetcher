76729599ac09af95380665cf783908d2e6e32884
Yasset Perez-Riverol <ypriverol@gmail.com>
2022-06-09 23:16:17 +0100

    Clean code

--- src/test/java/uk/ac/ebi/pride/pubmed/integration/PubMedFetcherTests.java
+++ src/test/java/uk/ac/ebi/pride/pubmed/integration/PubMedFetcherTests.java
@@ -2,8 +2,8 @@ package uk.ac.ebi.pride.pubmed.integration;
 
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+
+
 import org.springframework.util.Assert;
 import org.springframework.util.StringUtils;
 import uk.ac.ebi.pride.pubmed.PubMedFetcher;
